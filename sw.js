8fe7ff4ae1927b22ba7e8655c4cc594ad438e3e4
fdungdev-art <fdungdev@gmail.com>
2025-12-28 13:54:01 +0700

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,10 +1,23 @@
-self.addEventListener("install",e=>{
- e.waitUntil(
-  caches.open("stickwar-final").then(c=>c.addAll(["./","./index.html"]))
- );
+const cacheName = 'stickwar-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/game.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(filesToCache))
+  );
 });
-self.addEventListener("fetch",e=>{
- e.respondWith(
-  caches.match(e.request).then(r=>r||fetch(e.request))
- );
+
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
